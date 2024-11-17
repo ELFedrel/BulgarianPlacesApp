ac91ee1fe4a2dff3ab3aab3a683f4697c743cd4c
ELFedrel <ivodzhermanski@gmail.com>
2024-11-17 17:08:20 +0300

    Update Comment.cs

--- BulgarianPlacesApp.Web/Data/Models/Comment.cs
+++ BulgarianPlacesApp.Web/Data/Models/Comment.cs
@@ -1,6 +1,34 @@
-﻿namespace BulgarianPlacesApp.Web.Data.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulgarianPlacesApp.Web.Data.Models
 {
     public class Comment
     {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+       
+        public string Content { get; set; } = null!;
+
+
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+
+
+        [Required]
+        public Guid LocationPostId { get; set; }
+
+        [ForeignKey(nameof(LocationPostId))]
+        public LocationPost LocationPost { get; set; } = null!;
+
+
+        [Required]
+        public DateTime DateCreated { get; set; }
+
     }
 }

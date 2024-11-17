1b1be2057dfe24546fb4e18b38ba1d3c453ecc4b
ELFedrel <ivodzhermanski@gmail.com>
2024-11-17 17:09:12 +0300

    Update FavoriteViews.cs

--- BulgarianPlacesApp.Web/Data/Models/FavoriteViews.cs
+++ BulgarianPlacesApp.Web/Data/Models/FavoriteViews.cs
@@ -1,6 +1,20 @@
-﻿namespace BulgarianPlacesApp.Web.Data.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulgarianPlacesApp.Web.Data.Models
 {
+    [PrimaryKey(nameof(UserId), nameof(LocationId))]
     public class FavoriteViews
     {
+        [Required]
+        public Guid UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public ApplicationUser User { get; set; } = null!;
+
+        [Required]
+        public Guid LocationId { get; set; }
+        [ForeignKey(nameof(LocationId))]
+        public LocationPost Location { get; set; } = null!;
     }
 }
